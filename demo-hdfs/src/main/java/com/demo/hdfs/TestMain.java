f5cbb430e64678b0092c01590d8f2e8c52a5846e
admin <15082037343@163.com>
2020-07-29 09:48:42 +0800

    FileSystem

--- demo-hdfs/src/main/java/com/demo/hdfs/TestMain.java
+++ demo-hdfs/src/main/java/com/demo/hdfs/TestMain.java
@@ -1,18 +1,29 @@
 package com.demo.hdfs;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 
 import java.io.IOException;
-import java.util.Objects;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class TestMain {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
+//        Configuration conf = new Configuration();
+//        conf.addResource(Objects.requireNonNull(TestMain.class.getClassLoader().getResourceAsStream("conf/core-site.xml")));
+//        conf.addResource(Objects.requireNonNull(TestMain.class.getClassLoader().getResourceAsStream("conf/core-site.xml")));
+//        FileSystem fs = FileSystem.get(conf);
+
         Configuration conf = new Configuration();
-        conf.addResource(Objects.requireNonNull(TestMain.class.getClassLoader().getResourceAsStream("conf/core-site.xml")));
-        conf.addResource(Objects.requireNonNull(TestMain.class.getClassLoader().getResourceAsStream("conf/core-site.xml")));
-        FileSystem fs = FileSystem.get(conf);
+        FileSystem fs = FileSystem.get(new URI("hdfs://server:9000"), conf, "root");
+
+        FileStatus[] fileStatuses = fs.listStatus(new Path("/"));
+        for (FileStatus fileStatus : fileStatuses) {
+            System.out.println(fileStatus);
+        }
 
         fs.close();
     }
